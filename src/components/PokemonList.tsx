2a33feeb519ce98437876e362153dd3d198be85c
servetkarabas <servetkarabas@gmail.com>
2026-04-27 10:47:45 +0100

    pookemon list added

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,2 +1,18 @@
+import { usePokemons } from "../hooks/usePokemon";
 
+const PokemonList = () => {
+  const { data: pokemons } = usePokemons();
 
+  return (
+    <ul className="list bg-base-100 rounded-box shadow-md">
+      {pokemons?.map((p) => (
+        <li key={p.id} className="list-row">
+          {" "}
+          {p.name}{" "}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PokemonList;
